203d81f61103d203e1df06969b7983ba3b15b70b
kexiaojiu <kexiaojiu@163.com>
2017-07-26 22:42:54 +0800

    codes from chapter 02

---
+++ Chapter_02/nester_jacob/nester_jacob.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+"""这是nester.py模块，提供了一个名为print_lol()的函数，该函数可以打印列表"""
+def print_lol(the_list, level=0):
+    """位置参数the_list是列表，所指定的列表每个数据项会递归地输出到屏幕，各数据占一行"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level + 1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
+
+if __name__ == "__main__":
+    movies = [
+            "The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+            ["Graham Chapman",
+                ["Michael palin", "John Cheese", "Terry Gilliam"]]]
+    print_lol(movies)
+    #~ print_lol(movies, 2)
---
+++ Chapter_02/nester_jacob/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name            = 'nester_jacob',
+    version         = '1.1.0',
+    py_modules      = ['nester_jacob'],
+    author          = 'Jie, Ke',
+    author_email    = 'kejie@aliyun.com',
+    url             = 'https://github.com/kexiaojiu',
+    description     = 'A simple printer of nested lists',
+    )
